9c8fdef3c339fdb3a34f18014f78d6ad8cc3232d
薛晓轩 <1075654960@qq.com>
2020-08-30 17:01:22 +0800

    d

---
+++ 外观模式/Facade.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 外观模式
+{
+    class Facade
+    {
+        SubSystemOne one;
+        SubSystemTwo two;
+        SubSystemThree three;
+        SubSystemFour four;
+        public Facade()
+        {
+            one = new SubSystemOne();
+            two = new SubSystemTwo();
+            three = new SubSystemThree();
+            four = new SubSystemFour();
+        }
+        public void MethodA()
+        {
+            Console.WriteLine("");
+            one.MethodOne();
+            two.MethodTwo();
+            four.MethodFour();
+        }
+        public void MethodB()
+        {
+            Console.WriteLine("");
+            one.MethodOne();
+            two.MethodTwo();
+            three.MethodThree();
+        }
+    }
+}
---
+++ 外观模式/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace 外观模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Facade facade = new Facade();
+            facade.MethodA();
+            facade.MethodB();
+            Console.Read();
+        }
+    }
+}
