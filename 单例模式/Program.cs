7acf43c2cf887e6aa335b40c1790abdec8cc766b
薛晓轩 <1075654960@qq.com>
2020-09-02 16:22:25 +0800

    dl

---
+++ 单例模式/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace 单例模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
