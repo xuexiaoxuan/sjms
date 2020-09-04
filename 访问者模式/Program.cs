841b32dee97914de134de3caf765103d1e405b5d
薛晓轩 <1075654960@qq.com>
2020-09-04 11:26:49 +0800

    fwz

---
+++ 访问者模式/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace 访问者模式
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ObjectStructure o = new ObjectStructure();
+            o.Attach(new Man());
+            o.Attach(new Woman());
+            Success vl = new Success();
+            o.Display(vl);
+            
+        }
+    }
+}
